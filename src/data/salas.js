25378fe049dc300cacf8c01fdc7360f301c78c91
samusaas <samuelfortunato85@gmail.com>
2026-06-30 16:44:30 -0300

    TEST: ExploreView.vue

--- src/data/salas.js
+++ src/data/salas.js
@@ -1,9 +1,35 @@
-export const salas = [
+import { ref } from "vue"
+export const salas = ref[
 {
-    id: 1,
+    idSala: 1,
     nome: '22info22',
-    membros: 21,
-    
-
+    participantes: 21,
+    desc: 'fórum de discussão de sociologia da 2info2',
+    usuarioCriador: 'Samusaax',
+    status: 1
+},
+{
+    idSala: 2,
+    nome: 'Ciência todo dia',
+    participantes: 32,
+    desc: 'Discutindo física com kurt Cobain brasileiro',
+    usuarioCriador: 'PedroLooser',
+    status: 1
+},
+{
+    idSala: 3,
+    nome: 'Química do mal',
+    participantes: 2,
+    desc: 'Sala de discussão da 2quimiii',
+    usuarioCriador: 'Rochelixx',
+    status: 1
+},
+{
+    idSala: 4,
+    nome:'aaaaaa',
+    participantes: 56,
+    desc: 'AAAAAAAAaaa',
+    usuarioCriador: 'erererer',
+    status: 0,
 }
 ]
--- src/views/rooms/ExploreView.vue
+++ src/views/rooms/ExploreView.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { salas } from '@/data/salas';
+ defineProps(["idSala", "nome", "participantes", "desc", "usuarioCriador", "status"])
 </script>
 
 <template>
-  Explorar
+  <div class="container">
+    <h1>Explorar Salas</h1>
+    <section class="salas">
+      <salas v-for="sala in salas" :key="sala.idSala" :idSala="sala.idSala" :nome="sala.nome" :participantes="sala.participantes" :desc="sala.desc" :usuarioCriador="sala.usuarioCriador" :status="sala.status">
+      </salas>
+    </section>
+  </div>
 </template>
 
 <style scoped>
