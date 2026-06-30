23d768771ffb7caac9e91fe7a2ad771197541464
maysazvn <maysabmachado84@gmail.com>
2026-06-30 16:53:56 -0300

    FEAT: css header e sidebar

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -4,16 +4,17 @@
 <template>
   <header>
     <div>
-     <img src="" alt="Logo Header" class="logo">
+     <img src="../../../public/logo-kuaa.png" alt="Logo Header" class="logo">
     </div>
-    <div>
+    <div class="direito">
       <div class="search">
-        <font-awesome-icon icon="magnifying-glass"/>
+        <font-awesome-icon icon="magnifying-glass" class="lupa"/>
 
         <input type="search" v-model="busca" placeholder="Buscar..."> <!-- Busca de usuário, sala e postagem -->
       </div>
-
-      <font-awesome-icon icon="bars" />
+      <div>
+        <font-awesome-icon icon="bars" class="menu" />
+      </div>
     </div>
   </header>
 </template>
@@ -21,5 +22,24 @@
 <style scoped>
 header{
   background: #1E1E1E;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
+
+.search{
+  background: #F9EAA8;
+  border-radius: 10px;
+  padding: 2px;
+}
+
+.direito{
+  display: flex;
+  gap: 10px;
+}
+
+.menu{
+  color: #D9D9D9;
+}
+
 </style>
