94d72c2fbf578e81c6abc9132ce80c8d098f14b2
MouraHallana <kimimahina38@gmail.com>
2026-07-01 16:47:12 -0300

    FEAT: começo editar post e correção ID

--- src/components/layout/Postagens.vue
+++ src/components/layout/Postagens.vue
@@ -7,19 +7,22 @@ let postagens = ref([
     id: 1, titulo:'Esse é o bobby', conteudo: 'conteudo super importante', autor: 'usuariobobby', data: '24/06/2009'}
 ]);
 
- let usuario = ref('NN')
+ let usuario = ref('H')
 
 
-const postagensTituloNovo = ref('')
-const postagensConteudoNovo = ref('')
-const postagensAutorNovo = ref('')
+const postagensTituloNovo = ref('');
+const postagensConteudoNovo = ref('');
+const postagensAutorNovo = ref('');
 
 function adicionar() {
+    let maiorId =  Math.max(...postagens.value.map(item => item.id))
+
     const novoPost = {
         titulo: postagensTituloNovo.value,
      conteudo: postagensConteudoNovo.value,
         autor: postagensAutorNovo.value,
-        data:  Date(Date.now()).toLocaleString('pt-BR')
+        data:  Date(Date.now()).toLocaleString('pt-BR'),
+        id: maiorId.value + 1
     }
 
     postagens.value.unshift(novoPost)
@@ -29,33 +32,46 @@ function adicionar() {
     postagensTituloNovo.value = '';
 }
 
+
+
+
+
+
+
 function excluir(idItem) {
    postagens.value = postagens.value.filter(postagens => postagens.id !== idItem)
 }
 
+function editar(post) {
+    const edicao = prompt(`Edite o seu post!:`)
+
+    if (edicao !== null) {
+        post.conteudo = edicao.value
+    }
+}
 
 
 </script>
 
 <template>
 
+    <section>
+ 
     <div>
 
 
  <div class="postar">
+
         <h2>Criar post</h2>
 
         <input type="text" v-model="postagensTituloNovo" placeholder="Titulo"> 
         <input type="text" v-model="postagensAutorNovo" placeholder="Usuario">
         <textarea v-model="postagensConteudoNovo" placeholder="estou com dificuldade em..."></textarea>
         <button @click="adicionar">
-            Postar
+         Postar
         </button>
-
-
-
-
     </div>
+
  <div class="postagenss" v-for="post in postagens" :key="post.id" :usuario="usuario">
 
     <div class="listaPosts">
@@ -77,7 +93,7 @@ function excluir(idItem) {
      <div v-if="post.autor === usuario">
 
             <button @click.prevent="excluir(post.id)" >Excluir</button>
-            <button>Editar</button>
+            <button @click="editar(post.id)">Editar</button>
 
         </div>
           
@@ -88,7 +104,9 @@ function excluir(idItem) {
 
 
     </div>
-   
+
+
+    </section>
     
 </template>
 
@@ -98,6 +116,15 @@ div.listaPosts {
     border: solid 2px black;
     padding: 30px;
     max-width: 30%;
+    margin: 0 auto;
+    margin-bottom: 35px;
+}
+
+button {
+    border: SOLID black 2px;
+    border-radius: 25px;
+    padding: 10px;
+    margin-top: 10px;
 }
 
 input {
@@ -112,5 +139,7 @@ div.postar {
     display: flex;
     flex-direction: column;
     max-width: 30%;
+    margin: 0 auto;
+    margin-bottom: 30px;
 }
 </style>
