243b77349e86bb497f44ce14794a6b569c28846d
kellermannclara <clarakellermanndossantos@gmail.com>
2026-07-01 16:44:59 -0300

    FEAT (continuação de sala)

--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -1,13 +1,5 @@
 <script setup>
-
-import { useRouter } from 'vue-router';
-const router = useRouter()
-import { salas } from '@/data/salas';
-
-function irCriarSala() {
-  router.push('/room')
-}
-
+import { salas } from '@/data/salas'
 </script>
 
 <template>
@@ -15,65 +7,47 @@ function irCriarSala() {
     <nav class="geral">
       <ul>
         <li>
-          <router-link to="/">
-            <font-awesome-icon icon="house" /> Página Inicial
-          </router-link>
+          <router-link to="/"> <font-awesome-icon icon="house" /> Página Inicial </router-link>
         </li>
         <li>
-          <router-link to="/explore">
-            <font-awesome-icon icon="compass" /> Explorar
-          </router-link>
+          <router-link to="/explore"> <font-awesome-icon icon="compass" /> Explorar </router-link>
         </li>
         <li>
-          <router-link to="/profile">
-            <font-awesome-icon icon="user" /> Perfil
-          </router-link>
+          <router-link to="/profile"> <font-awesome-icon icon="user" /> Perfil </router-link>
         </li>
       </ul>
     </nav>
 
-    <hr>
+    <hr />
 
     <nav class="salas">
-<<<<<<< HEAD
-      <div>
-        <h1>SALAS</h1>  <!-- esse font-awesome-icon é o botão de mais ao lado do título salas que vai abrir o adicionar sala -->
-        <button @click="irCriarSala"> <font-awesome-icon icon="plus"/> </button>
 
-        <div> <!-- quem for criar as salas, já cria lá na pasta data um dataset de salas padrão pfv, pra deixar um v-for aqui. -->
+      <div class="criarSala">
+        <div class="criar">
+        <h1>SALAS</h1>
+        <router-link to="/room"> <font-awesome-icon icon="plus" class="mais" /> </router-link>
+        <!-- esse font-awesome-icon é o botão de mais ao lado do título salas que vai abrir o adicionar sala -->
+         </div>
 
+        <div class="salas">
           <div v-for="sala in salas" :key="sala.nome">
             <h2>{{ sala.nome }}</h2>
           </div>
-
-=======
-      <div class="criarSala">
-        <h1>SALAS</h1>
-        <font-awesome-icon icon="plus" class="icone-grupo" />
-        <!-- esse font-awesome-icon é o botão de mais ao lado do título salas que vai abrir o adicionar sala -->
-
-        <div>
           <!-- quem for criar as salas, já cria lá na pasta data um dataset de salas padrão pfv, pra deixar um v-for aqui. -->
->>>>>>> DEV
         </div>
-
       </div>
     </nav>
 
-    <hr>
+    <hr />
 
     <nav class="suporte">
       <h1>SUPORTE</h1>
       <ul>
         <li>
-          <router-link to="/help">
-            <font-awesome-icon icon="circle-question" /> Ajuda
-          </router-link>
+          <router-link to="/help"> <font-awesome-icon icon="circle-question" /> Ajuda </router-link>
         </li>
         <li>
-          <router-link to="/about">
-            <font-awesome-icon icon="circle-info" /> Sobre
-          </router-link>
+          <router-link to="/about"> <font-awesome-icon icon="circle-info" /> Sobre </router-link>
         </li>
       </ul>
     </nav>
@@ -92,20 +66,29 @@ aside.sidebar {
 }
 
 h1 {
-  color: #D9D9D9;
+  color: #d9d9d9;
   font-weight: bold;
 }
 
-.criarSala {
-  color: #D9D9D9;
+nav.salas{
+  display: flex;
+  flex-direction: column;
+}
+
+.criar {
+  color: #d9d9d9;
   display: flex;
   align-items: center;
   justify-content: space-between;
   gap: 5px;
 }
 
+div.salas{
+    color: #d9d9d9;
+}
+
 ul li a {
-  color: #D9D9D9
+  color: #d9d9d9;
 }
 
 ul li:hover {
@@ -117,12 +100,21 @@ ul li:hover {
   font-weight: bold;
 }
 
+.mais{
+  margin: 8px;
+}
+
+.router-link-active .mais{
+  transform: scale(1.4);
+  transition: .2s;
+}
+
 hr {
-  color: #D9D9D9;
+  color: #d9d9d9;
   margin: 2px;
 }
 
-.icone-grupo  {
+.icone-grupo {
   right: 0% !important;
 }
 </style>
--- src/views/rooms/RoomView.vue
+++ src/views/rooms/RoomView.vue
@@ -1,46 +1,46 @@
 <!-- read salas/comunidades -->
 <script setup>
-
-import { useRouter } from 'vue-router';
-import { salas } from '@/data/salas';
-import { ref } from 'vue';
-const router = useRouter();
-const nome = ref('');
-const descricao = ref('');
+import { useRouter } from 'vue-router'
+import { salas } from '@/data/salas'
+import { ref } from 'vue'
+const router = useRouter()
+const nome = ref('')
+const descricao = ref('')
 function cancelar() {
   router.push('/')
 }
 function criar() {
-  salas.value.push ({
+  salas.value.push({
     nome: nome.value,
-    descricao: descricao.value
+    descricao: descricao.value,
   })
-  console.log(salas.value);
+  console.log(salas.value)
 
-  nome.value = '';
-  descricao.value = '';
+  nome.value = ''
+  descricao.value = ''
 
-  router.push('/');
+  router.push('/')
 }
-
 </script>
 
 <template>
+  <div class="criarsala">
+    <h1>Criar Sala</h1>
+    <p>Nome da Sala</p>
+    <input v-model="nome" type="text" placeholder="blablabla" />
+    <p>Descrição</p>
+    <input v-model="descricao" type="text" placeholder="clallka" />
 
-  <h1>Criar Sala</h1>
-  <p>
-    Nome da Sala
-  </p>
-  <input v-model="nome" type="text" placeholder="blablabla">
-  <p>
-    Descrição
-  </p>
-  <input v-model="descricao" type="text" placeholder="clallka">
-
-  <button @click="criar">Criar</button> <button @click="cancelar" >Cancelar</button>
+    <button @click="criar">Criar</button> <button @click="cancelar">Cancelar</button>
+  </div>
 </template>
 
 <style scoped>
+.criarsala {
+  margin-left: 13rem;
+  text-align: center;
+}
+
 input {
   background-color: aqua;
   color: black;
