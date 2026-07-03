ddb92c86255251e612f7369d4a6832413c4cbc07
maysazvn <maysabmachado84@gmail.com>
2026-07-03 11:05:53 -0300

    FEAT: css pagina ajuda

--- src/views/suporte/HelpView.vue
+++ src/views/suporte/HelpView.vue
@@ -2,8 +2,22 @@
 </script>
 
 <template>
-  Ajuda
+  <main>
+    <h1>Ajuda do Kuaa</h1>
+    <div> 
+          <font-awesome-icon icon="angles-right" /> Pergunta
+
+          Resposta
+    </div>
+
+    <div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+div{
+  display: flex;
+  justify-content: center;
+}
 </style>
