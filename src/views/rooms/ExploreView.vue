afc2cbb0add4aea9a5e04f4a12ce54b254f4d83e
kellermannclara <clarakellermanndossantos@gmail.com>
2026-07-07 16:00:04 -0300

    FEAT(Fazendo o adicionar foto)

--- src/views/rooms/ExploreView.vue
+++ src/views/rooms/ExploreView.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { salas } from '@/data/salas'
 </script>
 
 <template>
+  <div v-for="sala in salas" :key="sala.nome" :nome="sala.nome" :descricao="sala.descricao" :banner="sala.banner" class="salas">
+    <h2>{{ sala.nome }}</h2>
+  </div>
   Explorar
 </template>
 
 <style scoped>
+.salas {
+  color: black;
+  text-align: center;
+}
 </style>
--- src/views/rooms/RoomView.vue
+++ src/views/rooms/RoomView.vue
@@ -6,6 +6,7 @@ import { ref } from 'vue'
 const router = useRouter()
 const nome = ref('')
 const descricao = ref('')
+const banner = ref('')
 function cancelar() {
   router.push('/')
 }
@@ -13,31 +14,56 @@ function criar() {
   salas.value.push({
     nome: nome.value,
     descricao: descricao.value,
+    banner: banner.value
   })
   console.log(salas.value)
 
   nome.value = ''
   descricao.value = ''
+  banner.value = ''
 
   router.push('/')
 }
+
 </script>
 
 <template>
-  <div class="criarsala">
-    <h1>Criar Sala</h1>
-    <p>Nome da Sala</p>
-    <input v-model="nome" type="text" placeholder="blablabla" />
-    <p>Descrição</p>
-    <input v-model="descricao" type="text" placeholder="clallka" />
+  <img class="imagem" :src="salas.banner" alt="Imagem do banner">
+  <h1 class="nome">Criar Sala</h1>
+  <section class="criarsala">
+    <div class="naosei">
+      <p>Nome da Sala</p>
+      <input v-model="nome" type="text" placeholder="blablabla" />
+      <p>Descrição</p>
+      <input v-model="descricao" type="text" placeholder="clallka" />
+    </div>
+    <input v-model="banner" placeholder="URL DA IMAGEM"> 
 
+  </section>
+  <div class="botoes">
     <button @click="criar">Criar</button> <button @click="cancelar">Cancelar</button>
   </div>
 </template>
 
 <style scoped>
-.criarsala {
+
+.nome {
+  text-align: center;
   margin-left: 13rem;
+}
+.imagem {
+  margin-left: 20vw;
+}
+
+.criarsala {
+  text-align: center;
+  margin-left: 13rem;
+}
+
+.criarsala p {
+  margin-right: 2vw;
+}
+.botoes {
   text-align: center;
 }
 
