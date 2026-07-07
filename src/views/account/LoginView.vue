780e6ba315febb6235eeef71b1463d6976f4b01e
Andrx223 <andrekosmala2009@gmail.com>
2026-07-07 14:19:18 -0300

    REFACTOR: Finalização páginas de cadastro e login

--- src/views/account/LoginView.vue
+++ src/views/account/LoginView.vue
@@ -1,7 +1,13 @@
 <script setup>
+
+//imports
+
 import { ref } from 'vue';
 import ButtonChild from '@/components/ButtonChild.vue';
-import { dadosLogin } from './login/dadosLogin';
+import { userReal } from './login/UserReal';
+import { emailReal } from './login/EmailReal';
+import { senhaReal } from './login/SenhaReal';
+
 // lets ////////
 
 let cadastro = ref(true);
@@ -9,12 +15,9 @@ let login = ref(false);
 let emailFalso = ref('');
 let userFalso = ref('');
 let senhaFalsa = ref('');
-let senhaReal = ref('');
-let emailReal = ref('');
-let userReal = ref('');
+
 
 // Functions /////
-
 
 function selecionarLogin(){
 if(cadastro.value == true){
@@ -31,17 +34,18 @@ function selecionarCadastro(){
 
 function enviar(email, senha, user){
   if(email.trim() !== '' && senha.trim() !== '' && user.trim() !== ''){
-dadosLogin.value.push(email)
-dadosLogin.value.push(senha)
-dadosLogin.value.push(user)
-
-emailReal.value = email
+ if(senhaReal.value.length == 0 && emailReal.value.length == 0 && userReal.value.length == 0){
+  emailReal.value = email
 userReal.value = user
 senhaReal.value = senha
 
 emailFalso.value = ''
 senhaFalsa.value = ''
 userFalso.value = '';
+ }else{
+  alert('Você já possui cadastro! Faça login!');
+ }
+
 }else{
   alert('Preencha todos os campos!');
 }
@@ -49,8 +53,10 @@ userFalso.value = '';
 function logar(email, senha){
   if(email === emailReal.value && senha === senhaReal.value){
     alert('Você logou com sucesso!')
+    senhaFalsa.value = '';
+    emailFalso.value = '';
   }else{
-    alert('Vai tomar no cu')
+    alert('Erro! Senha ou/e usuário inválido!')
   }
 }
 
@@ -58,30 +64,40 @@ function logar(email, senha){
 </script>
 
 <template>
-  <div class="container">
+  <div class="container"> 
+    <!-- essa ^ div é a geral que contem TUDO -->
     <div class="bemVindo">
+      <!-- essa ^ tem o texto de bem vindo e a imagem do kuaa -->
       <img src="../../../public/kuaa.png" alt="Kuaa Logo">
-      <p>
+      <p v-if="cadastro == true">
         <span>Bem vindo ao Kuaa!</span> 
 O Kuaa veio para ajudar você estudante a ter um desempenho melhor em seus estudos, provas e até mesmo fazer amigos novos!
 Faça seu cadastro hoje mesmo!
       </p>
+      <p v-if="login == true">
+        Bem vindo de volta ao Kuaa! 
+O Kuaa veio para ajudar você estudante a ter um desempenho melhor em seus estudos, provas e até mesmo fazer amigos novos!
+Faça seu login e encontre seus colegas novamente!
+      </p>
     </div>
     <div class="escolha">
+      <!-- essa ^ tem os botoes para alterar o login e cadastro -->
       <ButtonChild @clique="selecionarCadastro()">Cadastro</ButtonChild>
       <ButtonChild @clique="selecionarLogin()">Login</ButtonChild>
     </div>
 
     <div class="cadastro" v-show="cadastro == true">
+      <!-- essa ^ é a div que aparece quando você esta fazendo cadastro -->
       <label for="email"></label>
       <input type="email" name="email" placeholder="E-mail" required v-model="emailFalso">
       <input type="text" required v-model="userFalso" placeholder="Nome de usuário">
-      <input type="text" required v-model="senhaFalsa" placeholder="Senha">
+      <input type="password" required v-model="senhaFalsa" placeholder="Senha">
       <ButtonChild @clique="enviar(emailFalso, senhaFalsa, userFalso)">Cadastrar</ButtonChild>
     </div>
     <div class="login" v-show="login == true">
+      <!-- essa é a div ^ que aparece para fazer login -->
       <input type="email" name="email" placeholder="E-mail" required v-model="emailFalso">
-     <input type="text" required v-model="senhaFalsa" placeholder="Senha">
+     <input type="password" required v-model="senhaFalsa" placeholder="Senha">
      <ButtonChild @clique="logar(emailFalso, senhaFalsa)">
       Logar
      </ButtonChild>
