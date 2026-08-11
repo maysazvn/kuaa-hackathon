b28a9cdd5ae98edc63f5ebefece1d997a25db8ab
kellermannclara <clarakellermanndossantos@gmail.com>
2026-08-11 14:25:43 -0300

    FEAT: Imagem aparecendo no explore

--- src/components/cart/CartSala.vue
+++ src/components/cart/CartSala.vue
@@ -1,38 +1,39 @@
 <script setup>
- defineProps(["idSala", "nome", "participantes", "desc", "usuarioCriador", "status"])
+defineProps(['idSala', 'nome', 'participantes', 'desc', 'usuarioCriador', 'status', 'banner'])
 </script>
 <template>
-    <div class="lasa">
-        <div class="aa">
+  <div class="lasa">
+    <div class="aa">
+      <div class="aqFt">
+         <img :src="banner" :alt="nome">
 
-            <div class="aqFt">
-                <!-- só pra representar a foto que ainda não tá pronta -->
-                s
-            </div>
-            <h2>{{ nome }}</h2>
-        </div>
-        <p>{{ desc }}</p>
+        <!-- só pra representar a foto que ainda não tá pronta -->
+        
+      </div>
+      <h2>{{ nome }}</h2>
     </div>
+    <p>{{ desc }}</p>
+    <slot/>
+  </div>
 </template>
 
 <style>
-p{
-    margin: 25px;
+p {
+  margin: 25px;
 }
-.aa{
-    display: flex;
-    gap: 3vw;
-    align-items: center;
+.aa {
+  display: flex;
+  gap: 3vw;
+  align-items: center;
 }
-.lasa{
-    background-color: rgb(35, 142, 236);
-    height: 15vw;
+.lasa {
+  background-color: rgb(35, 142, 236);
+  height: 15vw;
 }
-.aqFt{
-    margin: 10px 0 0 30px;
-    background-color: rgb(255, 255, 255);
-    width: 6vw;
-    height: 4vw;
+.aqFt {
+  margin: 10px 0 0 30px;
+  background-color: rgb(255, 255, 255);
+  width: 6vw;
+  height: 4vw;
 }
-
 </style>
