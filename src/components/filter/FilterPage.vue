d96f62e577ffd988e5aa9ee1875eaee5f1e2d047
Frisk13108 <pietrof3108@gmail.com>
2026-08-12 16:52:33 -0300

    FEAT: Início lógica da pesquisa

---
+++ src/components/filter/FilterPage.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+import { ref, watch, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { salas } from '@/data/salas';
+
+const route = useRoute();
+
+const coisaPesquisada = ref('');
+
+watch(
+    () => route.query.dado,
+    (novoDado) => {
+        if(novoDado) {
+            coisaPesquisada.value = novoDado;
+        }
+    }
+)
+
+const salasFiltradas = computed(() => {
+    if(!coisaPesquisada.value)
+    return salas.filter(sala => sala.nome === coisaPesquisada.value);
+}
+)
+console.log(coisaPesquisada)
+
+</script>
+
+<template>
+    <div v-for="sala in salasFiltradas" :key="sala.idSala">
+        <div>
+            {{ sala.nome }}
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+
+</style>
