d555b3cafdcb7f4a98d274e3d64f4815e45b5644
kellermannclara <clarakellermanndossantos@gmail.com>
2026-08-13 18:51:24 -0300

    Tela de editar sala

--- src/views/rooms/EditSalaView.vue
+++ src/views/rooms/EditSalaView.vue
@@ -1,5 +1,36 @@
-<script setup></script>
+<script setup>
+import { salas } from '@/data/salas';
+import { ref, computed, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+const route = useRoute()
+const router = useRouter()
+const nome = ref('')
+const descricao = ref('')
+const banner = ref('')
+
+const sala = computed(() =>
+    salas.value.find(s => s.idSala == route.params.id)
+)
+
+function edit() {
+    sala.value.nome = nome.value
+    sala.value.desc = descricao.value
+    sala.value.banner = banner.value
+    router.push(`/salas/${route.params.id}`)
+}
+watch(sala, (novaSala) => {
+    if (novaSala) {
+        nome.value = novaSala.nome
+        descricao.value = novaSala.desc
+        banner.value = novaSala.banner
+    }
+}, { immediate: true })
+</script>
 <template>
     EDITE
+    <input v-model="nome">
+    <input v-model="descricao">
+    <input v-model="banner">
+    <button @click="edit">Salvar alterações</button>
 </template>
 <style scoped></style>
