a58fdb1ce8f878c3bf274d767ffaadd733c41380
Frisk13108 <pietrof3108@gmail.com>
2026-08-14 10:55:42 -0300

    FEAT: Criação dos users

---
+++ src/views/user/ProfileViewOther.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue';
+import { seguidores } from './Followers';
+import { seguindo } from './Following';
+import { users } from './Users';
+
+let existe = ref(true);
+</script>
+
+<template>
+    <main class="container" v-show="existe == true" >
+    <div class="cartaoPerfil">
+      <img v-if="urlBanner" :src="urlBanner" class="banner">
+      <img v-if="urlFoto" :src="urlFoto" class="foto">
+
+      <button class="seguirUsuario" v-on:click="seguir()">{{ mensagemSeguir }}</button>
+
+        <div>
+          <ul>
+            <li class="seguidores">
+              {{ seguidores.length }} Seguidores
+            </li>
+
+            <li class="seguindo">
+              {{ seguindo.length }} Seguindo
+            </li>
+          </ul>
+        </div>
+
+        <div class="salas" v-show="mostrarSala === 'sim'">
+          <ul>
+            <li v-for="sala in suarios" :key="sala.id" :nome="sala.nome">
+              <p>
+                {{ sala.nome }}
+              </p>
+            </li>
+          </ul> 
+        </div>
+
+
+      <h1>{{ nomeUsuario }}</h1>
+      <p>{{ desc }}</p>
+
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/user/Users.js
@@ -0,0 +1,23 @@
+export const users = [
+    {
+        id: 0,
+        nome: 'NeymarJr',
+        pfp: 'https://static.wikia.nocookie.net/famosos/images/d/d6/Neymar_-_Official_Portrait_FWC26_cropped.png/revision/latest?cb=20260614023953&path-prefix=pt-br',
+        banner: 'https://i.superesportes.com.br/Mma7tYFtvyrY4-HEGSiI4Q2-Zgk=/650x0/smart/imgsapp.mg.superesportes.com.br/app/noticia_126420360808/2022/11/22/3980504/neymar_1_87520.jpg',
+        desc: 'menino ney.'
+    },
+    {
+        id: 1,
+        nome: 'Krasue',
+        pfp: 'https://m.media-amazon.com/images/I/71Rblev0nXL.png',
+        banner: 'https://deadbydaylight.com/static/a197e7b8b44bfbe199546300338aeffb/691d2/MAPLE_Keyart_Resizes_WEBSITE_Keyart_1920x1080_V3_b1b5d017c9.png',
+        desc: 'uma mulher com hábitos noturnos exóticos'
+    },
+    {
+        id: 2,
+        nome: 'fbigatito',
+        pfp: 'https://chemitec.com.br/wp-content/uploads/2025/04/como-cuidar-de-gatos-filhotes.jpg',
+        banner: 'https://t4.ftcdn.net/jpg/05/40/20/91/360_F_540209110_YNGwMb4Q7zdqsYPmoP7gMnXtrXjKcIrS.jpg',
+        desc: 'apenas um gato normal.'
+    }
+];
