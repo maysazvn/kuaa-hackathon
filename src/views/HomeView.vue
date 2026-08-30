13d22a23a617c46b1c2a73257adb6ad0647da566
maysazvn <maysabmachado84@gmail.com>
2026-08-30 16:24:32 -0300

    ?

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -106,7 +106,6 @@ function criarPostRapido() {
   </div>
 </template>
 
-<<<<<<< Updated upstream
 <style scoped>
 .container {
   margin: 3vw 5vw auto;
@@ -272,5 +271,3 @@ CSS:
 
 */
 </style>
-=======
->>>>>>> Stashed changes
